7e96a99c79bf3d46395c2b117eed140a0d5a4aca
Ricardo Engelhard <72060532+ENGELHARD14@users.noreply.github.com>
2020-10-23 12:28:13 -0600

    Add files via upload

---
+++ ingresos.py
@@ -0,0 +1,14 @@
+class Ingresos:
+    def __init__(self):
+        self.ingresos = []
+
+    def Ingresar(self, monto):
+        ingreso = (id, monto)
+        self.ingresos.append(ingreso)
+
+    def MostrarI(self):
+        for ingreso in self.ingresos:
+            print("El monto ingresado es :" + ingreso)
+
+    def ReporteIngresos(self):
+        return self.ingresos
